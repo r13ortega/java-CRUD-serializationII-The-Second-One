66300edd3929e73cfbae4332164ed0f0165730af
Rey <reynaldoortega13@gmail.com>
2022-11-06 18:01:22 -0600

    copy over BoardGames Class from original project

---
+++ src/BoardGame.java
@@ -0,0 +1,33 @@
+public class BoardGame {
+    public String boardGameName;
+
+    public int minNumOfPlayers;
+
+    public int maxNumOfPlayers;
+
+    private boolean originalOwner;
+
+    public BoardGame(String boardGameName, int minNumOfPlayers, int maxNumOfPlayers, boolean originalOwner) {
+        this.boardGameName = boardGameName;
+        this.minNumOfPlayers = minNumOfPlayers;
+        this.maxNumOfPlayers = maxNumOfPlayers;
+        this.originalOwner = originalOwner;
+    }
+
+    @Override
+    public String toString() {
+        return "BoardGames{" +
+                "boardGameName='" + boardGameName + '\'' +
+                ", minNumOfPlayers=" + minNumOfPlayers +
+                ", maxNumOfPlayers=" + maxNumOfPlayers +
+                '}';
+    }
+
+    public boolean isOriginalOwner() {
+        return originalOwner;
+    }
+
+    public void setOriginalOwner(boolean originalOwner) {
+        this.originalOwner = originalOwner;
+    }
+}
